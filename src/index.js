f94b90e2fdf8bef585ee73f95290302d0d8b66fd
Stahnev Yury <stahnev@stael.ru>
2022-10-19 16:34:04 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import { initiateStore } from "./store/store";
+import * as actions from "./store/actions";
+
+const store = initiateStore();
+
+const App = () => {
+  const [state, setState] = useState(store.getState());
+
+  useEffect(() => {
+    store.subscribe(() => {
+      setState(store.getState);
+    });
+  }, []);
+
+  const completeTask = (taskId) => {
+    store.dispatch(actions.taskCompleted(taskId));
+  };
+
+  const changeTitle = (taskId) => {
+    store.dispatch(actions.titleChanged(taskId));
+  };
+
+  const deleteTask = (taskId) => {
+    store.dispatch(actions.taskDeleted(taskId));
+  };
+
+  return (
+    <>
+      {" "}
+      <h1>App</h1>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p>{`Completed: ${el.completed}`}</p>
+            <button onClick={() => completeTask(el.id)}>Complete</button>
+            <button onClick={() => changeTitle(el.id)}>Change title</button>
+            <button onClick={() => deleteTask(el.id)}>Delete task</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import { createStore } from "redux";
+import { taskReducer } from "./taskReducer";
+
+const initialState = [
+  { id: 1, title: "Task-1", completed: false },
+  { id: 2, title: "Task-2", completed: false },
+  { id: 3, title: "Task-3", completed: true },
+  { id: 4, title: "Task-4", completed: false },
+  { id: 5, title: "Task-5", completed: true },
+];
+
+export function initiateStore() {
+  return createStore(taskReducer, initialState);
+}
